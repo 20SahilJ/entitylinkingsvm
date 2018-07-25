a7b4a615552a6f5d3fb952e3efe176771d67ee57
20SahilJ <20sahilj@students.harker.org>
2018-07-24 23:18:32 -0700

    implemented confusion matrix

--- Entity Ranking.py
+++ Entity Ranking.py
@@ -1,14 +1,51 @@
 #all import statements
 import numpy as np
+import itertools
 import matplotlib.pylab as pl
+import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 #Note: A entity/mention set is defined to be the statistics derived from a single line in newdata.txt
 
@@ -164,10 +201,23 @@ for j in range(len(X_test)):
     '''
 #Calculates the a mean for each metric and prints them out
 averageaccuracy = metrics.accuracy_score(np.array(y_trues), np.array(y_preds))
-print('Average Accuracy', averageaccuracy)
+print('Accuracy', averageaccuracy)
 averageprecision = metrics.precision_score(np.array(y_trues), np.array(y_preds))
-print('Average Precision', averageprecision)
+print('Precision', averageprecision)
 averagef1 = metrics.f1_score(np.array(y_trues), np.array(y_preds))
-print('Average F1 Score', averagef1)
+print('F1 Score', averagef1)
 averagerecall = metrics.recall_score(np.array(y_trues), np.array(y_preds))
-print('Average Recall', averagerecall)
+print('Recall', averagerecall)
+
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_trues, y_preds)
+np.set_printoptions(precision=2)
+class_names = ['Incorrectly linked', 'Correctly linked']
+# Plot non-normalized confusion matrix
+print('Got here')
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+print('Got here 2')
+plt.show()
